a7d0643f4bcd1f51026fb512bca59986a94a8cfc
rever1300 <rever1300@gmail.com>
2021-04-09 11:54:33 +0200

    models.py modificated

--- formula1_project/Pages/models.py
+++ formula1_project/Pages/models.py
@@ -5,24 +5,42 @@ import formula1_project
 class Escuderia(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=400)
-    priority = models.IntegerField(default=1)
+    colors = models.CharField(max_length=100)
+    principalDriver = models.CharField(max_length=200)
+    secondaryDriver = models.CharField(max_length=200)
+    num_championships = models.IntegerField(max_length=100)
 
 class Piloto(models.Model):
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=400)
-    priority = models.IntegerField(default=1)
+    age = models.IntegerField(max_length=100)
+    nacionality = models.CharField(max_length=100)
+    escuderia = models.CharField(max_length=100)
+    height = models.IntegerField(max_length=100)
+    weight = models.IntegerField(max_length=100)
 
-class Estadistica(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=400)
-    priority = models.IntegerField(default=1)
+class EstadisticaDriver(models.Model):
+    nameDriver = models.CharField(max_length=200)
+    num_of_championships = models.IntegerField(max_length=100)
+    age_validity = models.IntegerField(max_length=100)
+    num_dif_teams = models.IntegerField(max_length=100)
+    name_dif_teams = models.CharField(max_length=100)
+
+class EstadisticaScuderia(models.Model):
+    nameScuderia = models.CharField(max_length=100)
+    age_of_birth = models.IntegerField(max_length=100)
+    num_of_championships = models.IntegerField(max_length=100)
 
 class Circuito(models.Model):
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=400)
-    priority = models.IntegerField(default=1)
+    country = models.CharField(max_length=100)
+    circuit_length = models.IntegerField(max_length=100)
+    laps_in_race = models.IntegerField(max_length=100)
+    first_gp = models.IntegerField(max_length=100)
+    race_distance = models.IntegerField(max_length=100)
+    lap_record = models.CharField(max_length=100)
 
 class Temporada(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.CharField(max_length=400)
-    priority = models.IntegerField(default=1)
+    year = models.IntegerField(max_length=100)
+    num_gp = models.IntegerField(max_length=100)
+    num_scuderias = models.IntegerField(max_length=100)
+    world_champion = models.CharField(max_length=100)
